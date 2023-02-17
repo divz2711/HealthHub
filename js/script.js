03fc91a847a0af5dd7850f6218d875087778b37d
divz2711 <121245222+divz2711@users.noreply.github.com>
2023-02-17 20:28:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,15 @@ function myOnClickFn(){
 function myOnClickFn(){
     document.location.href="contactus.html";
 }
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+  question.addEventListener('click', (e) => {
+    const answer = e.target.nextElementSibling;
+    if (answer.style.display === 'block') {
+      answer.style.display = 'none';
+    } else {
+      answer.style.display = 'block';
+    }
+  });
+})
